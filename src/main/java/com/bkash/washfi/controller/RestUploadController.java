9b8383327300a503e9354a9abecbb65c295bbca6
syed.ahmad <leowashfi@gmail.com>
2017-02-08 15:29:36 +0600

    1. CRUD implemented.

--- src/main/java/com/bkash/washfi/controller/RestUploadController.java
+++ src/main/java/com/bkash/washfi/controller/RestUploadController.java
@@ -1,8 +1,11 @@
 package com.bkash.washfi.controller;
 
+import com.bkash.washfi.entity.FileInformation;
 import com.bkash.washfi.entity.UploadModel;
+import com.bkash.washfi.service.UploadService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +17,7 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -27,8 +31,10 @@ import java.nio.file.Paths;
 public class RestUploadController {
 
     private final Logger logger = LoggerFactory.getLogger(RestUploadController.class);
-    private static String UPLOADED_FOLDER = "E://DemoDump//";
+    private static String UPLOADED_FOLDER = "E:\\DemoDump\\";
 
+    @Autowired
+    private UploadService uploadService;
 
     @GetMapping("/")
     public String index() {
@@ -40,33 +46,44 @@ public class RestUploadController {
     public ResponseEntity<?> multiUploadFileModel(@ModelAttribute UploadModel model) {
 
         logger.debug("Multiple file upload! With UploadModel");
-
+        FileInformation fileInformation=new FileInformation();
+        List<String> filePathList=new ArrayList<>();
         try {
 
-            saveUploadedFiles(Arrays.asList(model.getFiles()));
+            filePathList=saveUploadedFiles(Arrays.asList(model.getFiles()));
 
         } catch (IOException e) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
-        logger.info("text : "+model.getExtraField());
+        logger.info(" multiUploadFileModel : text :  "+model.getExtraField());
+        logger.info("multiUploadFileModel : List : size {0}"+filePathList.size());
+
+        fileInformation.setName(model.getExtraField());
+        fileInformation.setUrl(filePathList.toString());
+
+        uploadService.addInfo(fileInformation);
         return new ResponseEntity("Successfully uploaded!", HttpStatus.OK);
 
     }
 
-    private void saveUploadedFiles(List<MultipartFile> files) throws IOException {
-
+    private List<String> saveUploadedFiles(List<MultipartFile> files) throws IOException {
+        List<String> urlList=new ArrayList<>();
+        String url="";
         for (MultipartFile file : files) {
 
             if (file.isEmpty()) {
-                continue; //next pls
+                continue;
             }
 
             byte[] bytes = file.getBytes();
             Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
             Files.write(path, bytes);
 
+            url=UPLOADED_FOLDER+path.getFileName();
+            logger.info("saveUploadedFiles : Path : "+url);
+            urlList.add(url);
         }
-
+        return urlList ;
     }
 
 
